bd7697b78b36abfdd1e5306c2f9f982ef0b9475f
Yelena <31171709+yelenatm@users.noreply.github.com>
2019-05-19 23:34:35 +0600

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+
+
+module.exports = {
+    runtimeCompiler: true,
+    lintOnSave: 'error',
+    configureWebpack: {
+        plugins: []
+    },
+    productionSourceMap: false // turn off prod sourcemaps
+}
